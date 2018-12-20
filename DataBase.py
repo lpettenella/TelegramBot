c610b0f53921624629ac466eeaa41023e4c4c855
itsadmin <itsadmin@192.168.0.144>
2018-12-20 12:48:40 +0100

    Database

--- DataBase.py
+++ DataBase.py
@@ -1,24 +1,36 @@
 from Chat import Chat
 from Utente import Utente
+from dbhelper import DBHelper
+
 class DataBase():
     
+    db = DBHelper()
     lista_chat = list()
-    chatList = list()
+    chatList = db.get_chat("chat")
     utenti = list()
+    
+    for chat_id in chatList:
+        chat = Chat()
+        chat.chat_id = chat_id
+        mex = db.get_items("messaggi", "TESTO", chat_id)
+        chat.messaggi = mex
+        lista_chat.append(chat)
 
-    def addChat_id(self, chatid):
+    def addChat_id(self, chatid, db):
         if(self.chatList.__contains__(chatid)==False):
             self.chatList.append(chatid)
+            db.add_chat("chat", chatid)
             chat = Chat()
             chat.chat_id = chatid
             self.lista_chat.append(chat)
     
     
-    def addMex(self, chatid, mex):
+    def addMex(self, chatid, mex, db):
         for chats in self.lista_chat:
             if(chats.chat_id == chatid):
                 if(chats.messaggi.__contains__(mex)==False):
                     chats.messaggi.append(mex)
+                    db.add_item("messaggi", mex, chatid)
                     if len(chats.messaggi) >= 1000:
                         nuova = chats.messaggi[len(chats.messaggi)//2:]
                         chats.messaggi = nuova
@@ -26,7 +38,7 @@ class DataBase():
                     
                     
     
-    def addFoto(self, chatid, file_id):
+    def addFoto(self, chatid, file_id, db):
         for chats in self.lista_chat:
             if(chats.chat_id == chatid):
                 if(chats.foto.__contains__(file_id)==False):
