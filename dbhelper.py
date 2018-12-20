858f9d2cc980edf802ef801c0aae868c2b1ac339
itsadmin <itsadmin@192.168.0.144>
2018-12-20 16:16:56 +0100

    asd

--- dbhelper.py
+++ dbhelper.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 class DBHelper:
-    def __init__(self, dbname="bottest.sqlite"):
+    def __init__(self, dbname="botchat.sqlite"):
         self.dbname = dbname 
         self.conn = sqlite3.connect(dbname)
     
