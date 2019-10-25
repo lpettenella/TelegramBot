47ecb1a62a1541789e6330b397839c6cdd8ea1ac
Luca <45030042+lpettenella@users.noreply.github.com>
2019-10-25 09:13:10 +0200

    Update Chat.py

--- Chat.py
+++ Chat.py
@@ -3,4 +3,7 @@ class Chat:
         self.chat_id = 0
         self.messaggi = list()
         self.foto = list()
-    
+        self.gioco = False
+        self.mex_gioco = list()
+        self.mex_utenti = list()
+        self.gioco_soluzione = list()
